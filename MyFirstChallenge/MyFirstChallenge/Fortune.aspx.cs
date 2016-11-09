3ea7ac01f8c69e18d18c9b3e61b1e634f8a8967f
Joey Casto <castojr@yahoo.com>
2016-11-08 20:44:32 -0800

    Initial Commit

---
+++ MyFirstChallenge/MyFirstChallenge/Fortune.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace MyFirstChallenge
+{
+    public partial class Fortune : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       protected void fortuneButton_Click1(object sender, EventArgs e)
+        {
+            string age = ageTextBox.Text;
+            string pocketMoney = pocketTextBox.Text;
+
+            string result = "At " + age + " years old, I cannot believe you only have $" + pocketMoney + " in your pocket!";
+
+            fortuneLabel.Text = result;
+        }
+    }
+}
