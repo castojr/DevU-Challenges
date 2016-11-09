79a3afb54fd21636411283543be1723b0119101f
Joey Casto <castojr@yahoo.com>
2016-11-08 21:36:58 -0800

    Added RadioButtonChallenge

---
+++ RadioButtonChallenge/RadioButtonChallenge/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RadioButtonChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            if (pencilRadioButton.Checked)
+            {
+                resultLabel.Text = "You selected a pencil";
+                assetImage.ImageUrl = "pencil.png";
+            }
+            else if (penRadioButton.Checked)
+            {
+                resultLabel.Text = "You selected a pen";
+                assetImage.ImageUrl = "pen.png";
+            }
+            else if (phoneRadioButton.Checked)
+            {
+                resultLabel.Text = "You selected an iPhone 7";
+                assetImage.ImageUrl = "phone.png";
+            }
+            else if (tabletRadioButton.Checked)
+            {
+                resultLabel.Text = " Buy a Surface Book";
+                assetImage.ImageUrl = "tablet.png";
+            }
+            else
+            {
+                resultLabel.Text = "PLEASE CHOOSE A TOOL!";
+            }
+        }
+    }
+}
