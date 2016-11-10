1f9666be3acf66b1bbb420b521a7b9894f7479cc
Joey Casto <castojr@yahoo.com>
2016-11-09 21:20:27 -0800

    Added TimeSpan Challenge

---
+++ ChallengeDaysBetweenDates/ChallengeDaysBetweenDates/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeDaysBetweenDates
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            DateTime my1stCalendar = firstCalendar.SelectedDate;
+            DateTime my2ndCalendar = secondCalendar.SelectedDate;
+            TimeSpan span = new TimeSpan(my1stCalendar.Ticks - my2ndCalendar.Ticks);
+
+            resultLabel.Text = span.Days.ToString().Replace("-", "");
+        }
+    }
+}
