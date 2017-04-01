c5d18110c7aec373bb66056fd21c10c0763e6a39
Joey Casto <Joey Casto>
2017-03-31 20:44:32 -0700

    Updated Game class

--- ChallengeSimpleDarts/ChallengeSimpleDarts/Game.cs
+++ ChallengeSimpleDarts/ChallengeSimpleDarts/Game.cs
@@ -2,10 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Darts;
 
 namespace ChallengeSimpleDarts
 {
     public class Game
     {
+        private Player _player1;
+        private Player _player2;
+
+        private Random _random;
+        
+
+        public Game(string player1Name, string player2Name)
+        {
+            _player1 = new Player();
+            _player1.Name = player1Name;
+
+            _player2 = new Player();
+            _player2.Name = player2Name;
+            _random = new Random();
+        }
+
+        public void Play()
+        {
+            while (_player1.Score < 300 && _player2.Score < 300)
+            {
+                playRound(_player1);
+                playRound(_player2);
+            }
+        }
+
+        private void playRound(Player _player)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Dart dart = new Dart(_random);
+                dart.Throw();
+            }
+        }
     }
 }
