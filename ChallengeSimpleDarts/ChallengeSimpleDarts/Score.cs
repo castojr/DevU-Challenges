f2615c3f1335aaf4dca9e80b33ec5d106027a230
castojr <castojr@yahoo.com>
2017-04-04 21:05:37 -0700

    finalized score class

--- ChallengeSimpleDarts/ChallengeSimpleDarts/Score.cs
+++ ChallengeSimpleDarts/ChallengeSimpleDarts/Score.cs
@@ -11,7 +11,21 @@ namespace ChallengeSimpleDarts
     {
         public static void ScoreDart (Player player, Dart dart )
         {
-            player.Score = 0;
+            int score = 0;
+            if (dart.IsTriple)
+                score = dart.Score * 3;
+            else if (dart.IsDouble)
+                score = dart.Score * 2;
+            else
+                score = dart.Score;
+
+            if (dart.IsTriple && dart.Score == 0)
+                score = 50;
+            else if (dart.Score == 0)
+                score = 25;
+
+
+                player.Score = 0;
         }
     }
 }
