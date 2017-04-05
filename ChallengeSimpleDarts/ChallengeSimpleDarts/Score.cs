f1c0742adcb93e73852c3edba6913dad4982a39b
castojr <castojr@yahoo.com>
2017-04-04 20:56:59 -0700

    update score class

--- ChallengeSimpleDarts/ChallengeSimpleDarts/Score.cs
+++ ChallengeSimpleDarts/ChallengeSimpleDarts/Score.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ChallengeSimpleDartPlayer;
+using Darts;
 
 namespace ChallengeSimpleDarts
 {
     public class Score
     {
+        public static void ScoreDart (Player player, Dart dart )
+        {
+            player.Score = 0;
+        }
     }
 }
