c8381fa058b4c642603367c36442ad937a5e9a27
castojr <castojr@yahoo.com>
2017-04-24 21:19:13 -0700

    organized usings

--- ChallengeStudentCourses/ChallengeStudentCourses/Default.aspx.cs
+++ ChallengeStudentCourses/ChallengeStudentCourses/Default.aspx.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace ChallengeStudentCourses
 {
@@ -75,8 +71,8 @@ namespace ChallengeStudentCourses
 
             Dictionary<int, Student> students = new Dictionary<int, Student>()
             {
-                {1, new Student {Name = "Joey Casto", StudentId = 1, Courses = new List<Course> {course1, course2 }}},
-                {2, new Student {Name = "Ryan Casto", StudentId = 2, Courses = new List<Course> {course2, course3 }}}
+                {1, new Student {Name = "Joey Casto", StudentId = 1, Courses = new List<Course> {course1, course2}}},
+                {2, new Student {Name = "Ryan Casto", StudentId = 2, Courses = new List<Course> {course2, course3}}}
             };
 
             foreach (var student in students)
