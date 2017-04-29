f954bfff8be3fbdd3334790154d77b416b9917f0
castojr <castojr@yahoo.com>
2017-04-28 20:03:41 -0700

    Added Card Class

---
+++ MegaChallengeWar1/MegaChallengeWar1/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MegaChallengeWar1
+{
+    public class Card
+    {
+        public int CardNumber { get; set; }
+        public string CardSuit { get; set; }
+    }
+}
