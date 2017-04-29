eb306e78a4671dabf4e048d9dc7b5abab8f47eae
castojr <castojr@yahoo.com>
2017-04-28 21:06:09 -0700

    Completed Deck Dictionary

--- MegaChallengeWar1/MegaChallengeWar1/Default.aspx.cs
+++ MegaChallengeWar1/MegaChallengeWar1/Default.aspx.cs
@@ -36,11 +36,53 @@ namespace MegaChallengeWar1
                {11, new Card {CardNumber = 12, CardSuit = "Hearts", CardName = "Queen of Hearts" }},
                {12, new Card {CardNumber = 13, CardSuit = "Hearts", CardName = "King of Hearts" }},
                {13, new Card {CardNumber = 14, CardSuit = "Hearts", CardName = "Ace of Hearts" }},
+               {14, new Card {CardNumber = 2, CardSuit = "Diamonds", CardName = "2 of Diamonds" }},
+               {15, new Card {CardNumber = 3, CardSuit = "Diamonds", CardName = "3 of Diamonds" }},
+               {16, new Card {CardNumber = 4, CardSuit = "Diamonds", CardName = "4 of Diamonds" }},
+               {17, new Card {CardNumber = 5, CardSuit = "Diamonds", CardName = "5 of Diamonds" }},
+               {18, new Card {CardNumber = 6, CardSuit = "Diamonds", CardName = "6 of Diamonds" }},
+               {19, new Card {CardNumber = 7, CardSuit = "Diamonds", CardName = "7 of Diamonds" }},
+               {20, new Card {CardNumber = 8, CardSuit = "Diamonds", CardName = "8 of Diamonds" }},
+               {21, new Card {CardNumber = 9, CardSuit = "Diamonds", CardName = "9 of Diamonds" }},
+               {22, new Card {CardNumber = 10, CardSuit = "Diamonds", CardName = "10 of Diamonds" }},
+               {23, new Card {CardNumber = 11, CardSuit = "Diamonds", CardName = "Jack of Diamonds" }},
+               {24, new Card {CardNumber = 12, CardSuit = "Diamonds", CardName = "Queen of Diamonds" }},
+               {25, new Card {CardNumber = 13, CardSuit = "Diamonds", CardName = "King of Diamonds" }},
+               {26, new Card {CardNumber = 14, CardSuit = "Diamonds", CardName = "Ace of Diamonds" }},
+               {27, new Card {CardNumber = 2, CardSuit = "Clubs", CardName = "2 of Clubs" }},
+               {28, new Card {CardNumber = 3, CardSuit = "Clubs", CardName = "3 of Clubs" }},
+               {29, new Card {CardNumber = 4, CardSuit = "Clubs", CardName = "4 of Clubs" }},
+               {30, new Card {CardNumber = 5, CardSuit = "Clubs", CardName = "5 of Clubs" }},
+               {31, new Card {CardNumber = 6, CardSuit = "Clubs", CardName = "6 of Clubs" }},
+               {32, new Card {CardNumber = 7, CardSuit = "Clubs", CardName = "7 of Clubs" }},
+               {33, new Card {CardNumber = 8, CardSuit = "Clubs", CardName = "8 of Clubs" }},
+               {34, new Card {CardNumber = 9, CardSuit = "Clubs", CardName = "9 of Clubs" }},
+               {35, new Card {CardNumber = 10, CardSuit = "Clubs", CardName = "10 of Clubs" }},
+               {36, new Card {CardNumber = 11, CardSuit = "Clubs", CardName = "Jack of Clubs" }},
+               {37, new Card {CardNumber = 12, CardSuit = "Clubs", CardName = "Queen of Clubs" }},
+               {38, new Card {CardNumber = 13, CardSuit = "Clubs", CardName = "King of Clubs" }},
+               {39, new Card {CardNumber = 14, CardSuit = "Clubs", CardName = "Ace of Clubs" }},
+               {40, new Card {CardNumber = 2, CardSuit = "Spades", CardName = "2 of Spades" }},
+               {41, new Card {CardNumber = 3, CardSuit = "Spades", CardName = "3 of Spades" }},
+               {42, new Card {CardNumber = 4, CardSuit = "Spades", CardName = "4 of Spades" }},
+               {43, new Card {CardNumber = 5, CardSuit = "Spades", CardName = "5 of Spades" }},
+               {44, new Card {CardNumber = 6, CardSuit = "Spades", CardName = "6 of Spades" }},
+               {45, new Card {CardNumber = 7, CardSuit = "Spades", CardName = "7 of Spades" }},
+               {46, new Card {CardNumber = 8, CardSuit = "Spades", CardName = "8 of Spades" }},
+               {47, new Card {CardNumber = 9, CardSuit = "Spades", CardName = "9 of Spades" }},
+               {48, new Card {CardNumber = 10, CardSuit = "Spades", CardName = "10 of Spades" }},
+               {49, new Card {CardNumber = 11, CardSuit = "Spades", CardName = "Jack of Spades" }},
+               {50, new Card {CardNumber = 12, CardSuit = "Spades", CardName = "Queen of Spades" }},
+               {51, new Card {CardNumber = 13, CardSuit = "Spades", CardName = "King of Spades" }},
+               {52, new Card {CardNumber = 14, CardSuit = "Spades", CardName = "Ace of Spades" }},
 
 
            };
-
-            
+            // Displays all the Card names
+            foreach (var playerCard in Deck)
+            {
+                cardsDealtLabel.Text += String.Format("{0}</br>", playerCard.Value.CardName);
+            }        
 
 
         }
